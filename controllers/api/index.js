64bb9814ef32b71ef5509ceaa033ddba3e3fa538
brian-wastle <brian.wastle1@gmail.com>
2023-09-06 13:25:40 -0400

    added postRoutes to api routes for creating and deleting posts

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const reviewRoutes = require('./reviewRoutes');
 const threadRoutes = require('./threadRoutes');
+const postRoutes = require('./postRoutes');
 
 
 router.use('/users', userRoutes);
 router.use('/reviews', reviewRoutes);
 router.use('/threads', threadRoutes);
-
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -3,44 +3,38 @@ const { Users, Communities, Reviews, Threads, Posts, CommunityUsers } = require(
 const withAuth = require('../../utils/auth');
 
 
-//create a thread
-router.post('/', withAuth, async (req, res) => {
+//create a post on thread ":id", so the :id being passed as a parameter refers to the thread id, not a post id
+router.post('/:id', withAuth, async (req, res) => {
     try {
-      const newReview = await Threads.create({
+      const newPost = await Posts.create({
         ...req.body,
         user_id: req.session.user_id,
+        thread_id: req.params.id
       });
-  
-      res.status(200).json(newReview);
+
+      res.status(200).json(newPost);
     } catch (err) {
       res.status(400).json(err);
     }
   });
 
 
-//delete a thread
+//delete a post at post ":id", so the :id being passed as a paraemeter refers to the post id being destroyed
 router.delete('/:id', withAuth, async (req, res) => {
     try {
-      const threadData = await Threads.destroy({
+      const postData = await Posts.destroy({
         where: {
           id: req.params.id,
           user_id: req.session.user_id,
         },
       });
-
-      const postData = await Posts.destroy({
-        where: {
-          thread_id: req.params.id,
-          user_id: req.session.user_id,
-        },
-      });
   
-      if (!threadData) {
-        res.status(404).json({ message: 'No thread found with this id!' });
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
         return;
       }
   
-      res.status(200).json(threadData);
+      res.status(200).json(postData);
     } catch (err) {
       res.status(500).json(err);
     }
