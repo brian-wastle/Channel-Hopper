b55401b3ca701cea40915a5125a7bfd6d2bd1849
pb1983 <phil.bryer@gmail.com>
2023-09-07 16:15:08 -0400

    added navbar, font-family, and background colors

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./views/**/*.{handlebars,html,js}","./public/js/**/*.{handlebars,html,js}"],
+    theme: {
+      extend: {},
+        colors: 
+        {'vanilla': '#E9D8A6',
+        'alloy-orange': '#CA6702'}
+    },
+    plugins: [],
+  }
